cf7866e9da76d240a4d44607b7c0affe7fcfb984
seanrayment <rayment.code@gmail.com>
2017-05-25 17:13:45 -0400

    Initial Commit

---
+++ imessage-analytics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  imessage-analytics
+//
+//  Created by Sean Rayment on 5/25/17.
+//  Copyright © 2017 Sean Rayment. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ imessage-analytics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  imessage-analytics
+//
+//  Created by Sean Rayment on 5/25/17.
+//  Copyright © 2017 Sean Rayment. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
