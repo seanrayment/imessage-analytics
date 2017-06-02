0362461859880c6d24de9942fa54d58459c6d35d
lucasjurgensen <lucasjurgensen@lucass-mbp.home>
2017-06-01 22:45:31 -0400

    Added Conversation class

---
+++ Conversation.swift
@@ -0,0 +1,29 @@
+//
+//  Conversation.swift
+//  imessage-analytics
+//
+//  Created by Lucas Jurgensen on 6/1/17.
+//  Copyright © 2017 Sean Rayment. All rights reserved.
+//
+
+import Foundation
+
+
+class Conversation {
+    
+    var conversation = 
+    
+    // Initialize //
+    init(person: String) {
+        
+    }
+    
+    // Funcitons //
+    func LengthOfConversation() -> Int {
+        
+        return length
+    }
+    
+    
+    
+}
